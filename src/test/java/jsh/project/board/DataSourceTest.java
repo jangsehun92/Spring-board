ce710f1b9810116940f1d0ef961b3f7918b643c4
jangsehun92 <jangsehun92@gmail.com>
2020-03-28 07:32:34 +0900

    jdbc test code 추가

--- src/test/java/jsh/project/board/DataSourceTest.java
+++ src/test/java/jsh/project/board/DataSourceTest.java
@@ -7,23 +7,23 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations={"classpath:*/spring/root-context.xml"})
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context_test.xml")
 public class DataSourceTest {
+	private static final Logger logger = LoggerFactory.getLogger(DataSourceTest.class);
 
-	
 	@Inject
 	private DataSource ds;
 	
-	
 	@Test
-	public void testConnection() throws Exception {
-		Connection conn = ds.getConnection();
-		System.out.println("DB연결 성공!!");
+	public void 데이터소스_연결테스트() throws Exception {
+		Connection con = ds.getConnection();
+		logger.info("DataSource : "+con);
 	}
 	
 }
--- src/test/java/jsh/project/board/MybatisTest.java
+++ src/test/java/jsh/project/board/MybatisTest.java
@@ -6,25 +6,28 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context_test.xml")
 public class MybatisTest {
+	private static final Logger logger = LoggerFactory.getLogger(MybatisTest.class);
 
 	@Inject
 	private SqlSessionFactory sqlFactory;
 	
 	@Test
-	public void testFactory() throws Exception {
-		//SqlSessionFactory 객체 테스트
-		System.out.println("sqlFactory: " + sqlFactory);
+	public void sqlSessionFactory_테스트() throws Exception {
+		logger.info("sqlSessionFactory : " + sqlFactory);
 	}
+	
 	@Test
-	public void testSession() throws Exception {
+	public void sqlSession_테스트() throws Exception {
 		//SqlSession(MyBatis에 정의된 sql문 호출하는 객체) 객체 테스트
 		SqlSession session = sqlFactory.openSession();
-		System.out.println(session);
+		logger.info("sqlSesion : " + session);
 	}
 }

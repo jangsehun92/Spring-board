b647462783122d74d039e223ddbc9024dba7592f
jangsehun92 <jangsehun92@gmail.com>
2020-11-22 21:08:43 +0900

    remove setter

--- src/main/java/jsh/project/board/account/dto/response/ResponseAccountInfoDto.java
+++ src/main/java/jsh/project/board/account/dto/response/ResponseAccountInfoDto.java
@@ -20,19 +20,11 @@ public class ResponseAccountInfoDto {
 	public int getId() {
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 	
 	public String getNickname() {
 		return nickname;
 	}
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	
 	@Override
 	public String toString() {
 		return "ResponseAccountInfoDto { id : " + id + " nickname : " + nickname + " }";
